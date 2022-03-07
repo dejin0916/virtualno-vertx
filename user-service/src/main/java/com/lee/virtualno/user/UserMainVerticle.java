8b1014a24e5286e393323854c122a1a516c3848c
dejin0916 <dejin0916@163.com>
2022-03-07 21:16:29 +0800

    user服务初始化以及依赖调整

---
+++ user-service/src/main/java/com/lee/virtualno/user/UserMainVerticle.java
@@ -0,0 +1,11 @@
+package com.lee.virtualno.user;
+
+import io.reactivex.Completable;
+import io.vertx.reactivex.core.AbstractVerticle;
+
+public class UserMainVerticle extends AbstractVerticle {
+  @Override
+  public Completable rxStart() {
+    return super.rxStart();
+  }
+}
